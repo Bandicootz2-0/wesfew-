34d27e8737dc495b4a0c65ca273c7bc72674e215
Bandicootz2-0 <112668930+Bandicootz2-0@users.noreply.github.com>
2023-01-26 16:59:28 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+var bg,bg_img;
+var stickman;
+var obstacle;
+var obstacle2;
+
+function preLoad() {
+  bg_img= loadImage("Bg.png");
+}
+
+function setup() {
+createCanvas(500, 500);
+
+image(bg_img, width/2, height/2, width, height);
+
+
+}
+
+function draw() {
+//background("red");
+
+}
